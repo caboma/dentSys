e24eb8f4033ceac330e2a9995fbcfa100eccf753
Omar Cabatbat <omar_cabatbat@yahoo.com>
2023-05-28 00:30:26 -0400

    Setup Express Server for API

--- densysAPI/server.js
+++ densysAPI/server.js
@@ -1,4 +1,14 @@
-require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+const port = 8080 || 3001;
+
+app.use(cors());
+
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
